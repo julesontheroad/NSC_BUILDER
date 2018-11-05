21c9e48602833598adb28ff698c03c218da36266
julesontheroad <42461174+julesontheroad@users.noreply.github.com>
2018-11-05 20:49:46 +0100

    Add files via upload

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,9 +1,7 @@
 @ECHO OFF
-call "zconfig/nsp_cleaner_options.cmd"
-set pycommand=%pycommand:"=%
 ECHO.
 ECHO Installing dependencies 
 ECHO.
-%pycommand% -m pip install urllib3 unidecode tqdm bs4 tqdm requests image
+py -3 -m pip install urllib3 unidecode tqdm bs4 tqdm requests image
 ECHO.
 PAUSE
