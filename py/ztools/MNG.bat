de1c5801f3e62f3a105463629d7427b18bb7cde0
julesontheroad <42461174+julesontheroad@users.noreply.github.com>
2019-12-17 21:32:47 +0100

    Update version numbers

--- py/ztools/MNG.bat
+++ py/ztools/MNG.bat
@@ -264,7 +264,7 @@ ECHO =============================     BY JULESONTHEROAD     ===================
 ECHO -------------------------------------------------------------------------------------
 ECHO "                                POWERED BY SQUIRREL                                "
 ECHO "                    BASED ON THE WORK OF BLAWAR AND LUCA FRAGA                     "
-ECHO                                    VERSION 0.96d
+ECHO                                    VERSION 0.97
 ECHO -------------------------------------------------------------------------------------                   
 ECHO Program's github: https://github.com/julesontheroad/NSC_BUILDER
 ECHO Blawar's github:  https://github.com/blawar
