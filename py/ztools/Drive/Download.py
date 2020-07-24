3279d5e8c704185f3e615020ca86881337e6e71c
julesontheroad <42461174+julesontheroad@users.noreply.github.com>
2020-07-25 01:33:54 +0200

    avoid curses issues

--- py/ztools/Drive/Download.py
+++ py/ztools/Drive/Download.py
@@ -19,9 +19,6 @@ from Fs.Nacp import Nacp
 from Fs.pyNPDM import NPDM
 import nutdb
 from secondary import clear_Screen
-from python_pick import pick
-from python_pick import Picker
-from Interface import About
 from Drive.XciTools import supertrimm
 from Drive.Decompress import decompress
 import os
@@ -45,6 +42,25 @@ else:
 	
 remote_lib_file = os.path.join(zconfig_dir, 'remote_libraries.txt')
 download_lib_file = os.path.join(zconfig_dir, 'download_libraries.txt')
+
+def About():	
+	print('                                       __          _ __    __                         ')
+	print('                 ____  _____ ____     / /_  __  __(_) /___/ /__  _____                ')
+	print('                / __ \/ ___/ ___/    / __ \/ / / / / / __  / _ \/ ___/                ')
+	print('               / / / (__  ) /__     / /_/ / /_/ / / / /_/ /  __/ /                    ')
+	print('              /_/ /_/____/\___/____/_.___/\__,_/_/_/\__,_/\___/_/                     ')
+	print('                             /_____/                                                  ')
+	print('------------------------------------------------------------------------------------- ')
+	print('                        NINTENDO SWITCH CLEANER AND BUILDER                           ')
+	print('------------------------------------------------------------------------------------- ')
+	print('=============================     BY JULESONTHEROAD     ============================= ')
+	print('------------------------------------------------------------------------------------- ')
+	print('"                                POWERED BY SQUIRREL                                " ')
+	print('"                    BASED ON THE WORK OF BLAWAR AND LUCA FRAGA                     " ')
+	print('------------------------------------------------------------------------------------- ')                   
+	print("Program's github: https://github.com/julesontheroad/NSC_BUILDER                       ")
+	print('Cheats and Eshop information from nutdb and http://tinfoil.io                         ')
+	print('------------------------------------------------------------------------------------- ')
 
 def libraries(tfile):
 	db={}
@@ -108,6 +124,7 @@ class Section:
 		self.cryptoCounter = f.read(16)
 
 def pick_libraries():
+	from python_pick import Picker
 	title = 'Select libraries to search:  \n + Press space or right to select content \n + Press E to finish selection'
 	db=libraries(remote_lib_file)
 	if db==False:
@@ -135,6 +152,7 @@ def pick_libraries():
 	return paths,TDs	
 
 def pick_download_folder():
+	from python_pick import pick
 	title = 'Select download folder: '
 	db=libraries(download_lib_file)
 	if db==False:
@@ -216,6 +234,7 @@ def interface_file(path,TD):
 				return True
 
 def interface_filter(path=None):
+	from python_pick import pick
 	title = 'Add a search filter?: '
 	options = ['Yes','No']	
 	selected = pick(options, title, min_selection_count=1)
@@ -231,6 +250,7 @@ def interface_filter(path=None):
 		return ck
 		
 def interface_xci_mode():
+	from python_pick import pick
 	title = 'Xci files detected. Input download mode'
 	options = ['Untrimmed','Trimmed','Supertrimmed']	
 	selected = pick(options, title, min_selection_count=1)
@@ -238,6 +258,7 @@ def interface_xci_mode():
 	return response
 
 def interface_compressedf_mode():
+	from python_pick import pick
 	title = 'Compressed files detected. Input download mode'
 	options = ['Compressed','Uncompressed']	
 	selected = pick(options, title, min_selection_count=1)
